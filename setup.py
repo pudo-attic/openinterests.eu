604d2a650fc3bf40d6547ba881b209652aa6123a
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-16 23:21:03 +0100

    make a python package

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='opint',
+    version='0.1',
+    description="Explore power and influence in the European Union",
+    long_description='',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+    keywords='sql data procurement lobby power politics',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='http://pudo.org',
+    license='MIT',
+    packages=['opint'],
+    #packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+    ],
+    tests_require=[],
+    entry_points=\
+    """ """,
+)
