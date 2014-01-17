5e69300c91edbeb411d91bac49b63d44e410ce3e
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-17 11:59:39 +0100

    Port the basic components of the FTS extractor.

---
+++ sources/fts/download.py
@@ -0,0 +1,18 @@
+import requests
+import logging
+from datetime import datetime
+from sources.util import fetch_data
+
+log = logging.getLogger(__name__)
+BASE_URL = 'http://ec.europa.eu/budget/remote/fts/dl/export_%s_en.xml'
+
+
+def download():
+	for year in range(2007, datetime.now().year):
+		log.info("Downloading FTS for %s", year)
+		url = BASE_URL % year
+		fetch_data(url, 'fts/export_%s.xml' % year)
+
+
+if __name__ == '__main__':
+	download()
