fcd7cd5b6a30ec4e994c4a74238fe2c4650539eb
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-17 11:59:04 +0100

    Utilities for handling local data cache and DB connectivity.

---
+++ sources/util/db.py
@@ -0,0 +1,11 @@
+import os
+import dataset
+
+
+DATABASE_URI = os.environ.get('OPINT_ETL_DATABASE')
+assert DATABASE_URI, "No database URI defined in OPINT_ETL_DATABASE."
+
+engine = dataset.connect(DATABASE_URI)
+
+currency_table = engine['currency_rates']
+
