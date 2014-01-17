96f97202bb813319244d9dfff55fdd6982e0f54b
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-17 11:59:04 +0100

    Utilities for handling local data cache and DB connectivity.

---
+++ sources/util/__init__.py
@@ -0,0 +1,15 @@
+import logging
+from sources.util.db import engine
+from sources.util.cache import fetch_data, walk_path
+
+logging.basicConfig(level=logging.DEBUG)
+
+requests_log = logging.getLogger("requests")
+requests_log.setLevel(logging.WARNING)
+
+alembic_log = logging.getLogger("alembic")
+alembic_log.setLevel(logging.WARNING)
+
+
+
+
