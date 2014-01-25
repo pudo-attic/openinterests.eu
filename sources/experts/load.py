24f35ce4beaf6e5a48f48b5c8de01a33c57716d4
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-25 20:57:04 +0100

    Fix sources and a variety of front-end bugs.

--- sources/experts/load.py
+++ sources/experts/load.py
@@ -13,13 +13,16 @@ from sources.experts.util import exp_group_member
 
 log = logging.getLogger('sources.experts.load')
 EXPERTS_URL = 'http://ec.europa.eu/transparency/regexpert/index.cfm?do=transparency.showList'
-
+URL = 'http://ec.europa.eu/transparency/regexpert/index.cfm?do=groupDetail.groupDetail&groupID=%s'
 
 def load(loader, row):
     grp_id = row.pop('group_id')
+    url_id = int(grp_id[1:])
+    source_url = URL % url_id
 
     log.info("Loading: %s, %s", grp_id, row.get('name'))
-    grp = loader.make_entity(['public_body', 'organisation', 'expert_group'])
+    grp = loader.make_entity(['public_body', 'organisation', 'expert_group'],
+        source_url=source_url)
     grp.set('name', row.pop('name'))
     grp.set('abbreviation', row.pop('abbreviation'))
     grp.set('exp_id', grp_id)
@@ -29,20 +32,22 @@ def load(loader, row):
     grp.set('exp_active_since', row.pop('active_since'))
     grp.save()
 
-    lead_dg = loader.make_entity(['public_body'])
+    lead_dg = loader.make_entity(['public_body'], source_url=source_url)
     lead_dg.set('name', row.pop('lead_dg'))
     lead_dg.save()
 
-    rel = loader.make_relation('expert_group_associated', lead_dg, grp)
+    rel = loader.make_relation('expert_group_associated', lead_dg, grp,
+        source_url=source_url)
     rel.set('role', 'lead')
     rel.save()
 
     for d in exp_group_associated_dg.find(group_id=grp_id):
-        dg = loader.make_entity(['public_body'])
+        dg = loader.make_entity(['public_body'], source_url=source_url)
         dg.set('name', d.pop('dg'))
         dg.save()
 
-        rel = loader.make_relation('expert_group_associated', dg, grp)
+        rel = loader.make_relation('expert_group_associated', dg, grp,
+            source_url=source_url)
         rel.set('role', 'associated')
         rel.save()
 
@@ -58,11 +63,12 @@ def load(loader, row):
         else:
             schemata.append('person')
             
-        mem = loader.make_entity(schemata)
+        mem = loader.make_entity(schemata, source_url=source_url)
         mem.set('name', m.pop('name'))
         mem.save()
 
-        rel = loader.make_relation('expert_group_member', mem, grp)
+        rel = loader.make_relation('expert_group_member', mem, grp,
+            source_url=source_url)
         rel.set('exp_status', m.pop('status'))
         rel.set('exp_type', m.pop('member_type'))
         rel.save()
