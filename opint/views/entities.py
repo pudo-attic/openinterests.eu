150f504f53c201c7063e100a9f264a5be94e5a9f
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-25 23:25:51 +0100

    Customize the display of relationships.

--- opint/views/entities.py
+++ opint/views/entities.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, Markup
 from werkzeug.exceptions import NotFound
 
 from grano.service import search_entities
@@ -9,11 +9,20 @@ from grano.model import Entity
 entities = Blueprint('entities', __name__, static_folder='../static', template_folder='../templates')
 
 
+def render_relation(schema, direction, entity, relation):
+    path = 'relations/%s.%s.html' % (schema.name, direction)
+    if path not in entities.jinja_loader.list_templates():
+        path = 'relations/default.%s.html' % direction
+    return Markup(render_template(path, entity=entity,
+        relation=relation))
+
+
 @entities.route('/entities')
 def search():
     searcher = search_entities(request.args)
     pager = Pager(searcher, 'search')
     return render_template('search.html', searcher=searcher, pager=pager)
+
 
 @entities.route('/entities/<id>')
 def view(id):
@@ -32,4 +41,5 @@ def view(id):
         outbound_sections.append((schema, pager))
     return render_template('entity.html', entity=entity,
         inbound_sections=inbound_sections,
-        outbound_sections=outbound_sections)
+        outbound_sections=outbound_sections,
+        render_relation=render_relation)
