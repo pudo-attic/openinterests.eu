2c3ca9b49c30c2c134793af592f6217b0c055d39
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-25 10:32:56 +0100

    Very basic HTML integration and theme.

--- opint/manage.py
+++ opint/manage.py
@@ -1,5 +1,9 @@
 from grano.manage import manager
+from grano.core import app
 
+from opint.views.entities import entities
+
+app.register_blueprint(entities)
 
 if __name__ == "__main__":
     manager.run()
