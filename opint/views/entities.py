680bc1c9aa5985a80842f65937c24de67b912288
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-25 15:15:32 +0100

    404s for everyone!

--- opint/views/entities.py
+++ opint/views/entities.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from werkzeug.exceptions import NotFound
 
 from grano.service import search_entities
 from grano.lib.pager import Pager
@@ -11,10 +12,12 @@ entities = Blueprint('entities', __name__, static_folder='../static', template_f
 @entities.route('/entities')
 def search():
     searcher = search_entities(request.args)
-    pager = Pager(searcher)
+    pager = Pager(searcher, 'search')
     return render_template('search.html', searcher=searcher, pager=pager)
 
 @entities.route('/entities/<id>')
 def view(id):
     entity = Entity.by_id(id)
+    if entity is None:
+        raise NotFound()
     return render_template('entity.html', entity=entity)
