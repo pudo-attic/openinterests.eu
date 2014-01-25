3a3e7bb9f3d022b74c9d12b077466055b9df223e
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-25 15:15:32 +0100

    404s for everyone!

--- opint/views/relations.py
+++ opint/views/relations.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from werkzeug.exceptions import NotFound
 
 from grano.service import search_entities
 from grano.lib.pager import Pager
@@ -11,4 +12,6 @@ relations = Blueprint('relations', __name__, static_folder='../static', template
 @relations.route('/relations/<id>')
 def view(id):
     relation = Relation.by_id(id)
+    if relation is None:
+        raise NotFound()
     return render_template('relation.html', relation=relation)
