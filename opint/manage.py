c163ecf2cbd30907e2410db79a14f5829f2f4e4e
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-25 18:08:04 +0100

    Have a different module name.

--- opint/manage.py
+++ opint/manage.py
@@ -1,15 +1,5 @@
 from grano.manage import manager
-from grano.core import app
-
-from opint.views.entities import entities
-from opint.views.relations import relations
-from opint.views.base import base
-
-
-app.register_blueprint(entities)
-app.register_blueprint(relations)
-app.register_blueprint(base)
-
+from opint.web import app
 
 if __name__ == "__main__":
     manager.run()
