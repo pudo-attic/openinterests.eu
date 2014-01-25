57aa229b680e5d6c94a122bacca370f1af4820fd
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-25 13:21:30 +0100

    Search function implemented.

--- opint/manage.py
+++ opint/manage.py
@@ -2,8 +2,12 @@ from grano.manage import manager
 from grano.core import app
 
 from opint.views.entities import entities
+from opint.views.base import base
+
 
 app.register_blueprint(entities)
+app.register_blueprint(base)
+
 
 if __name__ == "__main__":
     manager.run()
