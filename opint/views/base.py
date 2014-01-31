e80a4ed0891ab748e90ae01381bfb1c1d3273137
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-31 19:04:27 +0100

    Remove sitemaps here.

--- opint/views/base.py
+++ opint/views/base.py
@@ -6,10 +6,6 @@ from opint.views.util import facet_schema_list
 
 
 base = Blueprint('base', __name__, static_folder='../static', template_folder='../templates')
-ROBOTS = """
-User-agent: *
-Sitemap: /sitemap.xml
-"""
 
 
 @base.route('/')
@@ -30,23 +26,3 @@ def about_page():
 @base.route('/data')
 def data_page():
     return render_template('data.html')
-
-
-@base.route('/robots.txt')
-def robots_txt():
-    res = make_response(ROBOTS)
-    res.headers['Content-Type'] = 'text/plain'
-    return res
-
-
-@base.route('/sitemap.xml')
-def sitemap_xml():
-    entities = [(entity.id, entity.updated_at, entity.degree) \
-        for entity in Entity.all().filter_by(same_as=None).limit(1000)]
-    upper = max([e[2] for e in entities])
-    entities = sorted(entities, key=lambda e: e[2], reverse=True)[:100]
-    entities = [(i, d.strftime('%Y-%m-%d'), '%.2f' % (float(s)/upper)) for (i,d,s) in entities]
-    res = make_response(render_template('sitemap.xml', entities=entities))
-    res.headers['Content-Type'] = 'text/xml'
-    return res
-
