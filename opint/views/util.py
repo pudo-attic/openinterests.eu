f478dee2499b305e4ab98345d071c0af1ef3d469
Friedrich Lindenberg <friedrich@pudo.org>
2014-02-19 00:24:40 +0100

    Minor bug fix to adapt with changing API.

--- opint/views/util.py
+++ opint/views/util.py
@@ -4,7 +4,7 @@ def facet_schema_list(obj, facets):
     results = []
     project = Project.by_slug('openinterests')
     for facet in facets:
-        schema = Schema.cached(project, obj, facet.get('term'))
+        schema = Schema.by_name(project, facet.get('term'))
         if not schema.hidden:
             results.append((schema, facet.get('count')))
     return results
